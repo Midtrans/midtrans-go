1b9af2271cd021cea0095245cc27f87ddcd20946
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-05-31 15:00:54 +0700

    Create const for banks

---
+++ bank.go
@@ -0,0 +1,30 @@
+package midtrans
+
+// Bank value
+type Bank string
+
+const (
+	//BankBni : bni
+	BankBni Bank = "bni"
+
+	//BankMandiri : mandiri
+	BankMandiri Bank = "mandiri"
+
+	//BankCimb : cimb
+	BankCimb Bank = "cimb"
+
+	//BankBca : bca
+	BankBca Bank = "bca"
+
+	//BankBri : bri
+	BankBri Bank = "bri"
+
+	//BankMaybank : maybank
+	BankMaybank Bank = "maybank"
+
+	//BankPermata : permata
+	BankPermata Bank = "permata"
+
+	//BankMega : mega
+	BankMega Bank = "mega"
+)
