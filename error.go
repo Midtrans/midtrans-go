f962badd4a816aab798690d85a3bf7a4b6602155
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-05-31 15:02:01 +0700

    Create Midtrans Error

---
+++ error.go
@@ -0,0 +1,28 @@
+package midtrans
+
+type Error struct {
+	Message string
+	StatusCode int
+	RawError error
+	RawApiResponse *ApiResponse
+}
+
+// GetMessage this get general message error when call api
+func (e *Error) GetMessage() string  {
+	return e.Message
+}
+
+// GetStatusCode this get api response status code coming from midtrans backend
+func (e *Error) GetStatusCode() int {
+	return e.StatusCode
+}
+
+// GetRawApiResponse this get api raw response from midtrans backend
+func (e *Error) GetRawApiResponse() *ApiResponse {
+	return e.RawApiResponse
+}
+
+// GetRawError GetRawApiResponse this get api raw response from midtrans backend
+func (e *Error) GetRawError() error {
+	return e.RawError
+}
