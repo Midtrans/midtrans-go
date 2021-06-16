c4c0d27d14abbd93e0e0438f5d1bce211258a848
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-06-16 14:27:50 +0700

    Update subscriptions api function details

--- coreapi/client.go
+++ coreapi/client.go
@@ -190,6 +190,8 @@ func GetBIN(binNumber string) (*BinResponse, *midtrans.Error) {
 	return getDefaultClient().GetBIN(binNumber)
 }
 
+// CreateSubscription : Do `/v1/subscriptions` To create subscription that contains all details for creating transaction
+// more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func (c Client) CreateSubscription(req *SubscriptionReq) (*CreateSubscriptionResponse, *midtrans.Error) {
 	resp := &CreateSubscriptionResponse{}
 	jsonReq, _ := json.Marshal(req)
@@ -206,10 +208,14 @@ func (c Client) CreateSubscription(req *SubscriptionReq) (*CreateSubscriptionRes
 	return resp, nil
 }
 
+// CreateSubscription : Do `/v1/subscriptions` To create subscription that contains all details for creating transaction
+// more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func CreateSubscription(req *SubscriptionReq) (*CreateSubscriptionResponse, *midtrans.Error) {
 	return getDefaultClient().CreateSubscription(req)
 }
 
+//GetSubscription : Do `/v1/subscriptions/{subscription_id}` To find subscription by id to see the subscription details
+// more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func (c Client) GetSubscription(subscriptionId string) (*StatusSubscriptionResponse, *midtrans.Error) {
 	resp := &StatusSubscriptionResponse{}
 	err := c.HttpClient.Call(
@@ -227,11 +233,14 @@ func (c Client) GetSubscription(subscriptionId string) (*StatusSubscriptionRespo
 	return resp, nil
 }
 
+//GetSubscription : Do `/v1/subscriptions/{subscription_id}` To find subscription by id to see the subscription details
+// more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func GetSubscription(subscriptionId string) (*StatusSubscriptionResponse, *midtrans.Error) {
 	return getDefaultClient().GetSubscription(subscriptionId)
 }
 
-
+// DisableSubscription : Do `/v1/subscriptions/{subscription_id}/disable` To make the subscription inactive
+// (the subscription will not create transaction anymore) more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func (c Client) DisableSubscription(subscriptionId string) (*DisableSubscriptionResponse, *midtrans.Error) {
 	resp := &DisableSubscriptionResponse{}
 	err := c.HttpClient.Call(
@@ -249,10 +258,14 @@ func (c Client) DisableSubscription(subscriptionId string) (*DisableSubscription
 	return resp, nil
 }
 
+// DisableSubscription : Do `/v1/subscriptions/{subscription_id}/disable` To make the subscription inactive
+// (the subscription will not create transaction anymore) more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func DisableSubscription(subscriptionId string) (*DisableSubscriptionResponse, *midtrans.Error) {
 	return getDefaultClient().DisableSubscription(subscriptionId)
 }
 
+// EnableSubscription : Do `/v1/subscriptions/{subscription_id}/enable` To make the subscription active
+// (the subscription will create periodic transaction) more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func (c Client) EnableSubscription(subscriptionId string) (*EnableSubscriptionResponse, *midtrans.Error) {
 	resp := &EnableSubscriptionResponse{}
 	err := c.HttpClient.Call(
@@ -270,10 +283,14 @@ func (c Client) EnableSubscription(subscriptionId string) (*EnableSubscriptionRe
 	return resp, nil
 }
 
+// EnableSubscription : Do `/v1/subscriptions/{subscription_id}/enable` To make the subscription active
+// (the subscription will create periodic transaction) more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func EnableSubscription(subscriptionId string) (*EnableSubscriptionResponse, *midtrans.Error) {
 	return getDefaultClient().EnableSubscription(subscriptionId)
 }
 
+// UpdateSubscription : Do `/v1/subscriptions/{subscription_id}` To update existing subscription details
+// more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func (c Client) UpdateSubscription(subscriptionId string, req *SubscriptionReq) (*UpdateSubscriptionResponse, *midtrans.Error) {
 	resp := &UpdateSubscriptionResponse{}
 	jsonReq, _ := json.Marshal(req)
@@ -290,6 +307,8 @@ func (c Client) UpdateSubscription(subscriptionId string, req *SubscriptionReq) 
 	return resp, nil
 }
 
+// UpdateSubscription : Do `/v1/subscriptions/{subscription_id}` To update existing subscription details
+// more detail refer to: http://api-docs.midtrans.com/#recurring-api
 func UpdateSubscription(subscriptionId string, req *SubscriptionReq) (*UpdateSubscriptionResponse, *midtrans.Error) {
 	return getDefaultClient().UpdateSubscription(subscriptionId, req)
 }
