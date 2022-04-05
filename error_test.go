659703c1345eccd3f967555205c2ccb107c906e2
Zaki Ibrahim <xaxxis.id@gmail.com>
2022-04-05 16:42:47 +0700

    add test for is and as

--- error_test.go
+++ error_test.go
@@ -17,20 +17,24 @@ func TestErrorStruct(t *testing.T) {
 		RawError:       errors.New("TEST FROM GO ERROR"),
 		RawApiResponse: nil,
 	}
+	var midError *Error
 	assert.Error(t, err)
+	assert.True(t, true, errors.Is(err, err))
+	assert.True(t, true, errors.As(err, &midError))
 	assert.Equal(t, `Error Test Message`, err.GetMessage())
 	assert.Equal(t, 200, err.GetStatusCode())
-	assert.Equal(t, `TEST FROM GO ERROR`, err.Error())
+	assert.Equal(t, "Error Test Message: TEST FROM GO ERROR", err.Error())
 }
 
 func TestErrorResponse(t *testing.T) {
-	serverKey := "dummy"
+	serverKey := "dummy "
 	c := GetHttpClient(Environment)
 	jsonReq, _ := json.Marshal("{\"transaction_details\": {\"order_id\": \"TEST-1648108994111\", \"gross_amount\": 10000}}")
 	err := c.Call(http.MethodPost, "https://app.midtrans.com/snap/v1/transactions", &serverKey, nil, bytes.NewBuffer(jsonReq), nil)
 
-	var midErr = err.Unwrap()
-	assert.True(t, true, errors.Is(err, midErr))
+	var midError *Error
+	assert.True(t, true, errors.Is(err, err))
+	assert.True(t, true, errors.As(err, &midError))
 	assert.Error(t, err)
 	assert.Equal(t, 401, err.StatusCode)
 	assert.Equal(t, "app.midtrans.com", err.RawApiResponse.Request.Host)
